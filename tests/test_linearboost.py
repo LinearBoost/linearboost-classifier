c17dd595e145097596992c1c5192b87c74e01ef5
Hamidreza Keshavarz <32555614+hamidkm9@users.noreply.github.com>
2025-05-25 20:56:18 +0200

    Update Test

--- tests/test_linearboost.py
+++ tests/test_linearboost.py
@@ -2,8 +2,14 @@ from sklearn.base import is_classifier
 
 from linearboost.linear_boost import LinearBoostClassifier
 
-from ._utils import check_estimator, get_expected_failed_tests
+from ._utils import check_estimator, get_expected_failed_tests, _yield_all_checks
 
+def get_expected_failed_tests(estimator):
+    failed = []
+    for check in _yield_all_checks(estimator):
+        if check.__name__ == "check_sample_weights_invariance":
+            failed.append(check)
+    return failed
 
 def test_linear_boost_estimator():
     """
