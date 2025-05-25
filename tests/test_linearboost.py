6deb14f307723188eebe38e387c41fb6fe6e9f19
Hamidreza Keshavarz <32555614+hamidkm9@users.noreply.github.com>
2025-05-25 21:17:23 +0200

    Update test_linearboost

--- tests/test_linearboost.py
+++ tests/test_linearboost.py
@@ -4,6 +4,7 @@ from linearboost.linear_boost import LinearBoostClassifier
 
 from ._utils import check_estimator, get_expected_failed_tests
 
+
 @parametrize_with_checks([LinearBoostClassifier()])
 def test_sklearn_compatible_estimator(estimator, check):
     check(estimator)
