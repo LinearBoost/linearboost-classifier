58513a1f085e12a4e463de60103b8c1787f8e32f
Hamidreza Keshavarz <32555614+hamidkm9@users.noreply.github.com>
2025-05-25 21:19:24 +0200

    Back to previous test

--- tests/test_linearboost.py
+++ tests/test_linearboost.py
@@ -1,7 +1,16 @@
-from sklearn.utils.estimator_checks import parametrize_with_checks
+from sklearn.base import is_classifier
+
 from linearboost.linear_boost import LinearBoostClassifier
 
+from ._utils import check_estimator, get_expected_failed_tests
 
-@parametrize_with_checks([LinearBoostClassifier()])
-def test_sklearn_compatible_estimator(estimator, check):
-    check(estimator)
+
+def test_linear_boost_estimator():
+    """
+    Test whether `LinearBoostClassifier` adheres to scikit-learn conventions.
+    """
+    check_estimator(
+        LinearBoostClassifier(),
+        expected_failed_checks=get_expected_failed_tests(LinearBoostClassifier()),
+    )
+    assert is_classifier(LinearBoostClassifier)
